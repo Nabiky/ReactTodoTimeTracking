ff838ebd595dad38f4fca1fd370dc5f8aaa52b08
Mireya Monzon <marte.pepen.mireya@gmail.com>
2017-02-25 20:33:24 +1300

    Init commit

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+var webpackConfig = require('./webpack.config.js');
+
+module.exports = function (config){
+  config.set({
+      browsers:['Chrome'],
+      singleRun: true,
+      frameworks:['mocha'],
+      files:[
+        'node_modules/jquery/dist/jquery.min.js',// the script loader is not avail in karma.
+        'node_modules/foundation-sites/dist/foundation.min.js',
+        'app/tests/**/*.test.jsx'
+      ],
+      preprocessors:{
+        'app/tests/**/*.test.jsx':['webpack','sourcemap']
+      },
+      reporters:['mocha'],
+      client:{
+        mocha:{
+          timeout: '5000'
+        }
+      },
+      webpack:webpackConfig,
+      webpackServer:{
+        noInfo:true
+      }
+  });
+
+};
